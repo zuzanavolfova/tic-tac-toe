397664da48752b3ca211bd2740ae030112e34cf7
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-30 09:04:16 +0200

    Add player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Player from "./components/Player";
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol>
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameElement = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerNameElement = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerNameElement}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={() => handleEditClick()}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
