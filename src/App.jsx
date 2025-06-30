32baf87c636bd847907033389a8005304e2052d7
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-06-30 13:49:44 +0200

    Add winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./assets/winning-combinations";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
